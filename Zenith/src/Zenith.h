a39b1387c209030d0ea7d1ae537df5e728b5b3be
Jonathan <72880844+ZENXDotPY@users.noreply.github.com>
2022-01-12 18:04:47 +0100

    Added basic logging

--- Zenith/src/Zenith.h
+++ Zenith/src/Zenith.h
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include "Zenith/Application.h"
+#include "Zenith/Log.h"
 
 // ---Entry Point----------------------
 #include "Zenith/Entrypoint.h"
--- Zenith/src/Zenith/Entrypoint.h
+++ Zenith/src/Zenith/Entrypoint.h
@@ -6,6 +6,8 @@ extern Zenith::Application* Zenith::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Zenith::Log::Init();
+
 	auto app = Zenith::CreateApplication();
 	app->Run();
 	delete app;
