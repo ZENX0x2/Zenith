ae386bed8f8954c024bd5a56048eaf7d221b15d1
Jonathan <72880844+ZENXDotPY@users.noreply.github.com>
2022-01-12 18:34:18 +0100

    ?

--- Zenith/src/Zenith/Entrypoint.h
+++ Zenith/src/Zenith/Entrypoint.h
@@ -7,7 +7,7 @@ extern Zenith::Application* Zenith::CreateApplication();
 int main(int argc, char** argv)
 {
 	Zenith::Log::Init();
-	ZT_CORE_WARN("Core Log Init");
+	ZT_CORE_WARN("Core Log Init");	
 	ZT_INFO("Client Log Init");
 
 	auto app = Zenith::CreateApplication();
